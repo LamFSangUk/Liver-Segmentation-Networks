80e0350cb81ba9f6f2263eba43253efae15c70e4
lamfsanguk <psu9808@gmail.com>
2020-05-31 00:54:17 +0900

    Implement VNet with Cutout Dropout

--- midl/networks/__init__.py
+++ midl/networks/__init__.py
@@ -1,7 +1,7 @@
 from .DenseVNet import DenseVNet
 from .VoxResNet import VoxResNet
 from .VNet import VNet
-from .TestNet import TestNet
+from .VNet_CD import VNet_CD
 from .VoxResNet_AG import VoxResNet_AG
 
-__all__ = [DenseVNet, VoxResNet, VNet, TestNet, VoxResNet_AG]
+__all__ = [DenseVNet, VoxResNet, VNet, VNet_CD, VoxResNet_AG]
