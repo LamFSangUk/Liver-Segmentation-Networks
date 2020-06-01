8e83273d88f083dea0c598b032f681a65461aff6
lamfsanguk <psu9808@gmail.com>
2020-06-01 20:23:14 +0900

    Implement VNet with Attentiongate

--- midl/networks/__init__.py
+++ midl/networks/__init__.py
@@ -1,7 +1,9 @@
 from .DenseVNet import DenseVNet
-from .VoxResNet import VoxResNet
 from .VNet import VNet
+from .VNet_AG import VNet_AG
 from .VNet_CD import VNet_CD
+from .VoxResNet import VoxResNet
 from .VoxResNet_AG import VoxResNet_AG
+from. VoxResNet_CD import VoxResNet_CD
 
-__all__ = [DenseVNet, VoxResNet, VNet, VNet_CD, VoxResNet_AG]
+__all__ = [DenseVNet, VoxResNet, VNet, VNet_AG, VNet_CD, VoxResNet_AG, VoxResNet_CD]
