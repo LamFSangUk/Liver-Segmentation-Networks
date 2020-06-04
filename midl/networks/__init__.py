77039b38a776d5b25b4a6aa08beab6c1d82b8696
lamfsanguk <psu9808@gmail.com>
2020-06-04 18:49:40 +0900

    Experiments for MPRBlock

--- midl/networks/__init__.py
+++ midl/networks/__init__.py
@@ -7,5 +7,7 @@ from .VoxResNet_AG import VoxResNet_AG
 from. VoxResNet_CD import VoxResNet_CD
 from. ROINet import ROINet
 from .TestNet import TestNet
+from .VNet_Dense import VNet_Dense
+from .VNet_MPR import VNet_MPR
 
-__all__ = [DenseVNet, VoxResNet, VNet, VNet_AG, VNet_CD, VoxResNet_AG, VoxResNet_CD, ROINet, TestNet]
+__all__ = [DenseVNet, VoxResNet, VNet, VNet_AG, VNet_CD, VoxResNet_AG, VoxResNet_CD, ROINet, TestNet, VNet_Dense, VNet_MPR ]
